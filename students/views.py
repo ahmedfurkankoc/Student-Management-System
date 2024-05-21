35cdaa29ff41d6a21e8e1f58afcb197779976053
Ahmed Furkan Koc <ahmedfurkankoc@gmail.com>
2024-05-21 16:04:15 +0300

    Student informations added and displayed

--- students/views.py
+++ students/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Student
 
 
@@ -6,3 +8,6 @@ def index(request):
     context = {'students': Student.objects.all()}
     return render(request, 'index.html', context)
 
+def view_student(request, pk):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
